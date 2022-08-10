f57798ddbaaa981d1540c79eb800a7fd88cc9f80
mcsdevbr <m.c.s@msn.com>
2022-08-09 21:40:23 -0300

    Iterating through a Api list and showing data accordingly

--- back/apps/core/serializers.py
+++ back/apps/core/serializers.py
@@ -4,10 +4,10 @@ from .models import List, Item
 class ItemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Item
-        fields = ['name', 'done']
+        fields = ['id', 'name', 'done']
 
 class ListSerializer(serializers.HyperlinkedModelSerializer):
     item_set = ItemSerializer(many=True)
     class Meta:
         model = List
-        fields = ['name', 'owner', 'url', 'item_set']
+        fields = ['id', 'name', 'owner', 'url', 'item_set']
--- front/frontend/src/components/UserList.js
+++ front/frontend/src/components/UserList.js
@@ -3,25 +3,26 @@ import List from "./List";
 
 
 class Userlist extends Component {
-    state = {list: null, loading: false}
+    state = {list: [], loading: false}
 
     async componentDidMount() {
         const config = {
             headers: {'Content-Type':'application/json'}
         }
         config.headers['Authorization'] = 'Token 92382be924bc368f91212a85f4a0064fe4cffd0a'
-        
+
         let url = 'http://localhost:8000/list/';
         const response = await fetch(url, config);
         const data = await response.json();
         console.log(data);
-        this.setState({list: data, loading: false})
+        this.setState({list: data, loading: false});
     }
 
     render() {
+        const listApi = this.state.list;
         return (
             <div>
-                <List listName={'Minha Lista'}/>
+                {listApi.map(list => <List key={ list.id} listName={list.name}/>)}
             </div>
         )
     }
