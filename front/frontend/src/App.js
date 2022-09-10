707c5a5e0e5e597b51284d452a6716dc43fd7163
mcsdevbr <m.c.s@msn.com>
2022-09-10 19:43:01 -0300

    clean file App.js

--- front/frontend/src/App.js
+++ front/frontend/src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import ListComponet from "./components/ListComponet";
-import UserListComponet from "./components/UserListComponet";
 import React from "react";
 
 function App() {
   return (
     <div>
-      <UserListComponet />
-      <ListComponet/>
+      App:)
     </div>
   );
 }
